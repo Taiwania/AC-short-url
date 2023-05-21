20dce007b7e83314fef70ae5d20ffad681309fe3
taiwania <taiwaniajusto@gmail.com>
2023-05-22 00:47:23 +0800

    Remove reutrn button and use the reflash message instead

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ app.get('/', (req, res) => {
 
 // Generate the short URL
 app.post('/', (req, res) => {
+  
+  // 檢查使用者是否將網址列留空，如果有，給出錯誤訊息
+  if (!req.body.url) {
+    const noUrlInput = `您沒有輸入網址，請重新整理後再輸入有效的網址。`
+    res.render('result', { result: noUrlInput })
+  }
 
   // Get the Long URL and suffix from the Input Box and suffix generator
   const longURL = req.body.url
@@ -49,20 +55,14 @@ app.post('/', (req, res) => {
   // Set the related message and button
   const copyButton = `<button class="btn btn-success" type="submit">複製</button>`
   const shortUrl = `${URL}:${port}/${suffix}`
-  const successMsg = `短網址產生完畢：${shortUrl}`
-
-  // 檢查使用者是否將網址列留空，如果有，給出錯誤訊息
-  if (longURL.length === 0) {
-    const noUrlInput = `您沒有輸入網址，請重新輸入。`
-    res.render('result', { result: noUrlInput })
-  }
+  const successMsg = `短網址產生完畢：${shortUrl}<br>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。`
 
   // 檢查輸入的網址是否在資料庫有紀錄
   ShortUrl.findOne({ url: longURL })
     .then((result) => {
       // 如果有則給出產生過的短網址
       if (result) {
-        const recordMsg = `您輸入的網址已產生過這個短網址：${URL}:${port}/${result.suffix}`
+        const recordMsg = `您輸入的網址已產生過這個短網址：${URL}:${port}/${result.suffix}<br>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。`
         res.render('result', { result: recordMsg, copy: copyButton })
       } else {
         return ShortUrl.create({ url: longURL, suffix }) // 如果沒有則將輸入的網址及產生的短網址後綴輸入資料庫並給出對映短網址
