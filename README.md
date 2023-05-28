994129969ca42ad7b606badd977b01cae21bfefb
taiwania <taiwaniajusto@gmail.com>
2023-05-29 03:17:26 +0800

    Fix the result page to do a short url link that can linked via a herf

--- README.md
+++ README.md
@@ -1,11 +1,44 @@
 # 短網址產生器
 
 ## 專案簡介
-本專案網站可以將使用者輸入的過長網址，轉成短網址。本專案尚在構建中。
+本專案網站可以將使用者輸入的過長網址，轉成短網址。
+
+## 網站畫面
 
 ## 使用技術
-1.  MongoDB Atlas
-2.  Express
+1.  Express 4.18.2
+2.  Express-Handlebars 7.0.7
+3.  Mongoose 7.1.1
+
+## 安裝流程
+1.  打開 Terminal，cd 至預定存放本專案的目錄資料夾，執行 clone 程序：
+   
+    ```git clone https://github.com/Taiwania/AC-restaurant-list```
+
+2.  切換到專案資料夾並安裝 npm：
+    
+    ```
+    cd AC-restaurant-list
+    npm install
+    ```
+
+3.  使用 MongoDB Atlas，在自己的資料庫內建立一個「restaurant」資料庫，並將自己的連線連結存成「.env」檔案放到專案目錄資料夾。
+
+4.  執行以下指令，匯入本專案的種子資料到 MongoDB 資料庫：
+
+    ```npm run seed```
+
+    看到以下訊息代表資料已經匯入 MongoDB：
+
+    ```The data of restaurants is imported.```
+
+5.  最後執行以下指令啟動網站：
+
+    ```npm run dev```
+
+    看到以下指令代表網站載入完成，可以使用瀏覽器打開 http://localhost:3000 即可進入網站。
+
+    ```The website http://localhost:3000 is online.```
 
 ## 開發人員
 Taiwania
--- routes/modules/result.js
+++ routes/modules/result.js
@@ -42,11 +42,11 @@ router.post("/", async (req, res) => {
         // 如果有則給出產生過的短網址
         if (result) {
           const shortUrl = `${URL}/${result.suffix}`;
-          const recordMsg = `您輸入的網址曾產生出這個短網址：<p class="fw-bold" id="shortUrl">${shortUrl}</p>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。`;
+          const recordMsg = `您輸入的網址曾產生出這個短網址：<p><a href="${shortUrl}" class="fw-bold" id="shortUrl">${shortUrl}</a></p><p>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。</p>`;
           res.render("result", { result: recordMsg, copy: copyButton });
         } else {
           const shortUrl = newShortUrl;
-          const successMsg = `短網址產生完畢：<p class="fw-bold" id="shortUrl">${shortUrl}</p>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。`;
+          const successMsg = `短網址產生完畢：<p><a href="${shortUrl}" class="fw-bold" id="shortUrl">${shortUrl}</a></p><p>如果要重新輸入網址，請使用瀏覽器的重新整理按鈕。</p>`;
           return ShortUrl.create({ url: longURL, suffix }) // 如果沒有則將輸入的網址及產生的短網址後綴輸入資料庫並給出對映短網址
             .then(() =>
               res.render("result", { result: successMsg, copy: copyButton })
